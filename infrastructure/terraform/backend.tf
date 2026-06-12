908c115578925824213a39cea88e0f4e1ffb0ecb
chase-linux24 <cealy95@gmail.com>
2026-06-12 18:50:18 -0500

    Fix backend to use dynamodb_table, pin Terraform 1.5.7

--- infrastructure/terraform/backend.tf
+++ infrastructure/terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket       = "devops-challenge2-terraform-state-381491979307"
-    key          = "tech-challenge1-5/terraform.tfstate"
-    region       = "us-east-2"
-    use_lockfile = true
-    encrypt      = true
+    bucket         = "devops-challenge2-terraform-state-381491979307"
+    key            = "tech-challenge1-5/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "devops-challenge2-terraform-locks"
+    encrypt        = true
   }
 }
